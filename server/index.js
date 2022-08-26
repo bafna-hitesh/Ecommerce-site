3ebed70cfa1ecb5ea68d829bac5589765bd9a582
Hitesh Bafna <hitesh.bafna00@gmail.com>
2022-08-26 17:16:34 +0530

    fix: cross origin

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+const cors = require('./allowCors');
 
 const { initializeDBConnection } = require('./db/db.connect')
 const { PopulateProducts } = require('./models/product.model')
