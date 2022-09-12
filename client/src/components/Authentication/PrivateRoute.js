2da31dcae818b196084817e13cfbde3acf948e89
Hitesh Bafna <hitesh.bafna00@gmail.com>
2022-09-12 18:06:58 +0530

    feat: Add Notify to private route

--- client/src/components/Authentication/PrivateRoute.js
+++ client/src/components/Authentication/PrivateRoute.js
@@ -1,5 +1,6 @@
 import { Outlet, Navigate } from 'react-router-dom';
 import { useAuth } from '../../context/AuthContext';
+import toast from 'react-hot-toast';
 
 export const PrivateRoute = ({ path, ...props }) => {
    const {
@@ -9,6 +10,7 @@ export const PrivateRoute = ({ path, ...props }) => {
    return token ? (
       <Outlet {...props} path={path} />
    ) : (
+      toast.error('Please Login To Continue...'),
       <Navigate state={{ from: path }} replace to='/login' />
    );
 };
